4c5734eadc5e28889008cb7de1913e505261352c
MateusValentim <mateus.valentim19@hotmail.com>
2020-06-18 00:25:10 -0300

    metodo delete - porém ainda dando erro na hora do teste a API do site está com erro também - aguardar retorno deles

--- src/test/java/TesteApi.java
+++ src/test/java/TesteApi.java
@@ -7,6 +7,7 @@ import static org.hamcrest.Matchers.*;
 
 public class TesteApi {
 
+	String vote_id;
 	// testando API de cadastro
 
 	@Test
@@ -35,7 +36,27 @@ public class TesteApi {
 		System.out.println("Retorno => " + response.body().asString());
 		String id = response.jsonPath().getString("id");
 
+		vote_id = id;
 		System.out.println("ID => " + id);
+	}
+
+	@Test
+	public void deletaVotacao() {
+		votacao();
+		deletarVoto();
+	}
+
+	private void deletarVoto() {
+		String url = "https://api.thecatapi.com/v1/votes/{vote_id}";
+
+		Response response = given().contentType("application/json")
+				.header("x-api-key", "e4c41546-a216-4002-8d82-53a575d52017").pathParam(vote_id, vote_id).when()
+				.delete("https://api.thecatapi.com/v1/votes/{vote_id}");
+
+		System.out.println("Retorno Deletar Voto =>" + response.body().asString());
+
+		response.then().statusCode(200).body("message", containsString("SUCCESS"));
+
 	}
 
 }
